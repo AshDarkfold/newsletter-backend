2aec0107c454e86f927c49cb4ff379c08102c49a
AshDarkfold <ashutosh.kaushik2018@vitstudent.ac.in>
2021-06-28 16:35:24 +0530

    add: models and crud routes

--- newsletter/models.py
+++ newsletter/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Hero(models.Model):
     name = models.CharField(max_length=60)
-    email = models.CharField(max_length=254)
-    # isSubscribed = models.Boolean
+    email = models.CharField(max_length=254, unique=True)
+    isSubscribed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
